81d51bee32ae2ed590bbee1e42e813224be73ec4
lyk-hlfcdboi <amlyk@qq.com>
2024-04-07 21:20:43 -0700

    Update: Apirl 7 2024

--- Pages/account_management.py
+++ Pages/account_management.py
@@ -11,17 +11,18 @@ def fetch_user_loans(user_id):
     """Fetches loan information for a specific user."""
     FIREBASE_URL = HashingURL.get_user_path(HashingURL.hash_user(int(user_id)))
 
-    response = requests.get(f"{FIREBASE_URL}/{user_id}/loans.json")
+    response = requests.get(f"{FIREBASE_URL}/{user_id}/books.json")
     if response.ok:
-        loans = response.json()
-    return loans
+        if isinstance(response.json(), dict):
+            return []
+    return response.json()
 
 def format_and_sort_loans(loans):
     """Formats loans data and sorts it by status, borrow on, and due to."""
     df = pd.DataFrame(loans)
     df = df[["book_id","start_date","due_date","status"]]
     df.columns = ['Book ID', 'Start Date', 'Return Date', 'Status']
-    df.sort_values(by = ['Status', 'Start Date', 'Return Date'], inplace=True)
+    df.sort_values(by = ['Start Date', 'Return Date', 'Status'], inplace=True)
     return df
 
 def account_management_page():
@@ -32,20 +33,21 @@ def account_management_page():
         loans = fetch_user_loans(user_id)
         if loans:
             df = format_and_sort_loans(loans)
-            df_html = df.to_html(index=False)
+            df_html = df.head(15).to_html(index=False)
+            st.title('Borrow&Return Records')
             st.write(df_html, unsafe_allow_html=True)
             st.write(f"Number of books borrowed: {len(loans)}")
             st.write(f"Number of books borrowing: {len(df[df['Status'] == 'borrowing'])}")
             st.write(f"Number of books returned: {len(df[df['Status'].isin(['returned', 'overdue returned'])])}")
         else:
-            st.write("No loan records found.")
+            st.write("No borrow/return record found.")
 
         if st.button('Log Out'):
             # Clear user-specific session state variables
             st.session_state.clear()
             st.success("You have been logged out.")
     else:
-        st.error("Please login first.")
+        st.warning("Please login first.")
         user_auth.render_login_signup()
 
 # Example call to this function; you would typically do this from your main app script
--- Pages/search_book.py
+++ Pages/search_book.py
@@ -1,31 +1,86 @@
 import streamlit as st
 import requests
 import pandas as pd
+import json
+from requests.exceptions import JSONDecodeError
+from Pages.HashingURL import hash_book, get_book_path
+
+def adjust_input(full_name):
+	parts = full_name.split()
+	adjusted_parts = [part[0].upper() + part[1:].lower() for part in parts]
+	adjusted_name = " ".join(adjusted_parts)
+	return adjusted_name
+
+bookURLS = {
+    0: "https://books-1-25ca2-default-rtdb.firebaseio.com/books",
+    1: "https://books-2-87c03-default-rtdb.firebaseio.com/books",
+    2: "https://books-3-82c7c-default-rtdb.firebaseio.com/books",
+    3: "https://books-4-b4971-default-rtdb.firebaseio.com/books",
+    4: "https://books-5-20414-default-rtdb.firebaseio.com/books"
+}
+
 
 # Function to perform the search
 def search_books(book_title, author_name):
     # Firebase Database URL
-    db_url = 'https://library551-default-rtdb.firebaseio.com/Book.json'
+    # book_title = adjust_input(book_title)
+    author_name = adjust_input(author_name)
     results = {}; 
 
     # Search by Book Title (Exact Match)
     if book_title:
-        query = f'orderBy="title"&equalTo="{book_title}"'
-        response = requests.get(f'{db_url}?{query}')
-        books = response.json().items()
-        for book_id, info in books.items():
-            results[book_id] = info
+        query_params = {
+            'orderBy': json.dumps('title'),
+            'equalTo': json.dumps(book_title)
+        }
+
+        for url in bookURLS.values():
+            response = requests.get(f"{url}.json", params = query_params)
+            if response.status_code == 200:
+                try:
+                    books = response.json()
+                    if books:
+                        for book_id, info in books.items():
+                            results[book_id] = info
+                except JSONDecodeError:
+                    # Handle the case where the response body isn't valid JSON
+                    print(f"Invalid JSON response from {url}")
+                    continue
+            else:
+                # Handle failed requests
+                print(f"Failed to fetch data from {url}, status code: {response.status_code}")
+
 
 
     # Search by Author (Exact and Contains)
     if author_name:
-        query = f'orderBy="title"&equalTo="{author_name}"'
-        response = requests.get(f'{db_url}?{query}')
-        for book_id, book_info in books.items():
-            if author_name in book_info.get("author", []):  # Checks if author_name is in the authors list
-                results[book_id] =  book_info
-
-    return results
+        name_kw = author_name.split()
+        print(name_kw)
+        for nkw in name_kw:
+            for url in bookURLS.values():   
+                response = requests.get(f"{url.strip('books')}/authorKeyWord/{nkw}.json")
+                if response.status_code == 200:
+                    try:
+                        dict_bid = response.json()
+                        if dict_bid:
+                            for id_ in dict_bid.keys():
+                                url_ = get_book_path(hash_book(int(id_)))
+                                resp = requests.get(f"{url_}/{id_}.json")
+                                if resp.status_code == 200 and resp.json():
+                                    results[id_] = resp.json()
+                                else:
+                                    continue
+                    except JSONDecodeError:
+                        # Handle the case where the response body isn't valid JSON
+                        print(f"Invalid JSON response from {url}")
+                        continue
+                else:
+                    # Handle failed requests
+                    print(f"Failed to fetch data from {url}, status code: {response.status_code}")
+    inter = False
+    if author_name and book_title:
+        inter = True
+    return results, inter
 
 def render_search_page():
     st.title("Search Books")
@@ -37,26 +92,33 @@ def render_search_page():
     # Search button
     if st.button('Search'):
         # Perform the search
-        search_results = search_books(book_title, author_name)
-
+        search_results, inter = search_books(book_title, author_name)
+        
+        if not book_title and not author_name:
+            st.warning("Please enter a book title or an author name to perform the search.")
+            return
         if search_results:
             data = []
             
             # Process and display results in a table
-            for book_id, info in search_results:
-                print(info)
+            for book_id, info in search_results.items():
+                authors = info.get("authors", [])
+                if authors:
+                    authors = [a.replace(',', '') for a in authors.keys()] 
+                subjects = info['subjects']
                 d_ = {
                     "ID": book_id,
                     "Title": info["title"],
-                    "Authors": ", ".join(info.get("authors", [])),
-                    "Genres": ", ".join(info.get("subjects", [])),
+                    "Authors": ", ".join(authors),
+                    "Genres": ', '.join(subjects) if isinstance(subjects, list) else subjects,
                     "In Stock": info.get("availableNums", 0)
                 }
                 data.append(d_)
 
             df = pd.DataFrame(data)
-            df.reset_index(drop = True, inplace = True)
-            st.table(df)
+            df_html = df.head(15).to_html(index=False)
+            st.title('Search Results')
+            st.write(df_html, unsafe_allow_html=True)
         else:
             st.error("Your book is not found.")
 
